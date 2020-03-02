02e5521a252b58257400a9e03ed8982957be741b
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-03-02 11:11:35 +0100

    added start script for heroku

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.listen(
+  port,
+  console.log(
+    `Portal ${port} boot up sequence initiated, Stargate link to homeworld confirmed!`
+  )
+);

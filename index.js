61e787a48b5d304f50bbe797fa0801788ac15bd0
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-03-02 11:51:26 +0100

    added cors and body parser

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const db = require("./db");
 const imageRouter = require("./image/router");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+const corsMiddleware = cors();
+const jsonParser = bodyParser.json();
+
+app.use(corsMiddleware);
+app.use(jsonParser);
 app.use(imageRouter);
+
 const port = process.env.PORT || 4000;
 
 app.listen(
